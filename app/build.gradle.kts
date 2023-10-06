b5194709ba1e0af449ec5221d93756951c3aa8ec
Vu Tue Nam <namvt.bi12-304@st.usth.edu.vn>
2023-10-07 01:08:07 +0700

    fix firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.8.1")
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
